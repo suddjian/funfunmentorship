c4232b180ef076fb4363a47825ce8e47b7ecab5e
David Aaron Suddjian <aasuddjian@gmail.com>
2017-09-16 02:36:28 -0700

    shitty untested code

--- src/users.js
+++ src/users.js
@@ -1,4 +1,6 @@
 
+import { snakeCase } from 'lodash'
+
 const benignErrorCodes = [
   'warming_up'
 ]
@@ -48,4 +50,29 @@ export const makeFetchUsers = (fetcher) => async (attempts=0) => {
   // TODO map/filter asynchronously so as not to hog the event loop
   return data.map(formatUser)
   .filter(user => !!user.mentorship)
+}
+
+export const extractSkills = (users) => {
+  const skills = users.reduce((skills, user) => {
+    user.mentorship[group].forEach(skillName => {
+      const skillId = snakeCase(skillName)
+      if (!skills[skillId]) {
+        skills[skillId] = {
+          skillId,
+          nameVotes: {}, // TODO heapify nameVotes
+          seeking: [],
+          offering: []
+        }
+      }
+      const skill = skills[skillId]
+      skill[group].push(user.username)
+      skill.nameVotes[skillName] = (skill.nameVotes[skillName] || 0) + 1
+    })
+    return skills
+  }, {})
+  const skillNames = Object.keys(skills)
+  return {
+    skillNames,
+    skills: skillNames.map(skillName => skills[skillName])
+  }
 }
--- test/users.js
+++ test/users.js
@@ -1,13 +1,21 @@
 import { expect } from 'chai'
 import { cloneDeep } from 'lodash'
 import { mpj, DavDavDavid } from './fixtures'
-import { formatUser } from '../src/users'
+import { formatUser, makeFetchUsers } from '../src/users'
 
 describe('user utils', () => {
 
   it('should format the user data correctly', () => {
-    const test = user => expect(formatUser(user.rawData())).to.deep.equal(user.formattedData())
+    const test = user => expect(formatUser(user.rawData()))
+      .to.deep.equal(user.formattedData())
     test(mpj)
     test(DavDavDavid)
+  })
+
+  it('should filter users from the raw endpoint json', async () => {
+    const fetchUsers = makeFetchUsers(() =>
+      [ mpj.rawData(), DavDavDavid.rawData() ]
+    )
+    expect(await fetchUsers()).to.deep.equal([DavDavDavid.formattedData()])
   })
 })
