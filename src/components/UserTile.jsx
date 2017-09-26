3c2027b2d96fe353ae2c033db01aa74f42e4b98f
Konstantin Ivanov <konstantin@studiolye.se>
2017-09-26 16:25:00 +0200

    - New styling - TODO: Link location to usermap

--- src/components/UserTile.jsx
+++ src/components/UserTile.jsx
@@ -1,31 +1,60 @@
 
 import React from 'react'
 import style from './UserTile.less'
+import classNames from 'classnames'
+import Geopin from './Geopin'
 
 import { getProfileUrl } from '../users'
 
-const getTitlePic = getProfileUrl(28)
+const getTitlePic = getProfileUrl(160) // making it crisp for them retina screens
 
 const SkillList = ({ className, title, list }) => (
-  <div>
-    <h3 className={`title ${style.skillTitle}`}>{title}</h3>
-    <ul className={style.skillList}>
+  <div className={style.skillsCategory}>
+    <h3 className={`title ${style.skillsCategoryTitle}`}>{title}</h3>
+    <ul className={style.skills}>
       {list.map(tech =>
-        <li key={tech} className={style.skill}>{tech}</li>
+        <li key={tech} className={[`skill skill-${title}`]}>{tech}</li>
       )}
     </ul>
   </div>
 )
 
-export default ({user}) => (
-  <div className={style.userTile}>
-    <span className={style.username}>
-      <a className={style.userLink} href={user.url}>
-        <img className='profile-tiny' src={getTitlePic(user.username)} />
-        {user.username}
+const Location = ({user}) => {
+  if (user.location) {
+    return (
+      <a className={style.userLocation}>
+        <Geopin />
+        {user.location.lat}, {user.location.lng}
       </a>
-    </span>
-    <SkillList title='Seeking' list={user.mentorship.seeking} />
-    <SkillList title='Offering' list={user.mentorship.offering} />
-  </div>
+    )
+  } else {
+    return null
+  }
+}
+
+const Caption = ({user}) => {
+  if (user.location) {
+    return <span className={style.userCaption}>{user.location.caption}</span>
+  } else {
+    return null
+  }
+}
+
+export default ({user}) => (
+  <article className={style.userTile}>
+    <section className={style.userInfo}>
+      <span className={style.username}>
+        <a className={style.userLink} href={user.url}>
+          <img className='profile-tiny' src={getTitlePic(user.username)} />
+          <span className={style.userName}>{user.username}</span>
+        </a>
+        <Location user={user} />
+        <Caption user={user} />
+      </span>
+    </section>
+    <section className={style.skillArea}>
+      <SkillList title='Seeking' list={user.mentorship.seeking} />
+      <SkillList title='Offering' list={user.mentorship.offering} />
+    </section>
+  </article>
 )
