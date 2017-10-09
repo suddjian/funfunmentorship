1c453dc1a20671f8ea79653b5c57d9ff251dc1ac
Andrew-Morris <blatadude@gmail.com>
2017-10-08 22:19:59 -0700

    Read the docs more carefully, OfflinePlugin might actually work now

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,10 @@ module.exports = {
     new webpack.NamedModulesPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
     new OfflinePlugin({
-      publicPath: process.env.NODE_ENV === 'prod' ? '/funfunmentorship/' : '/' 
+      publicPath: process.env.NODE_ENV === 'prod' ? '/funfunmentorship/' : '/',
+      Serviceworker: {
+        publicPath: process.env.NODE_ENV === 'prod' ? '/funfunmentorship/' : '/', 
+      },
     })
 
   ],
